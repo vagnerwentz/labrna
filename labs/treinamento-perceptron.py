73e108753a97ca6273ea42e0f209fc0adfc64acd
davips <dpsabc@gmail.com>
2025-04-29 15:55:33 -0300

    Using for loop to simplify handling of several ys in perceptron lab

--- labs/treinamento-perceptron.py
+++ labs/treinamento-perceptron.py
@@ -1,25 +1,13 @@
 import numpy as np
 
-from labrna.perceptron import Perceptron
+from src.labrna.perceptron import Perceptron
 
 X = np.array([[0, 0],
               [0, 1],
               [1, 0],
               [1, 1]])
-y_E = np.array([0,
-                0,
-                0,
-                1])
-y_OU = np.array([0,
-                 1,
-                 1,
-                 1])
-
-perceptron_E = Perceptron(dimensionalidade=2)
-perceptron_E.aprender(X, y_E)
-
-perceptron_OU = Perceptron(dimensionalidade=2)
-perceptron_OU.aprender(X, y_OU)
-
-print("Predições E:", perceptron_E.predizer(X))
-print("Predições OU:", perceptron_OU.predizer(X))
+ys = ["0 0 0 1", "0 1 1 1", "0 1 1 0", "1 0 0 1"]
+for y_str in ys:
+    p = Perceptron(dimensionalidade=2)
+    p.aprender(X, np.fromstring(y_str, dtype=int, sep=' '))
+    print(p.predizer(X))
