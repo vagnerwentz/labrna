993169ba3a006e9b7508289ef277d224bb74a9ba
davips <dpsabc@gmail.com>
2025-04-29 10:17:20 -0300

    Add activation functions plot

---
+++ labs/funcoes-de-ativacao.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn.functional as F
+
+# Entrada de exemplo
+x = torch.linspace(-10, 10, 100)
+
+# Funções de ativação
+linear = x
+degrau = (x >= 0).float()
+sigmoide = torch.sigmoid(x)
+tanh = torch.tanh(x)
+relu = F.relu(x)
+
+# Exibir ou plotar os resultados (exemplo com matplotlib)
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 6))
+plt.plot(x, linear, label='Linear')
+plt.plot(x, degrau, label='Degrau')
+plt.plot(x, sigmoide, label='Sigmoide Logística')
+plt.plot(x, tanh, label='Tangente Hiperbólica')
+plt.plot(x, relu, label='ReLU (Linear Retificada)')
+plt.legend()
+plt.grid(True)
+plt.title('Funções de Ativação')
+plt.show()
